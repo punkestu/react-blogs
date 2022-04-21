4e03a76d652e8f65389e9705590845762ca851e7
punkestu <pangestubima89@gmail.com>
2022-04-21 11:00:23 +0700

    adding navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 // import logo from './logo.svg';
 import './App.css';
 
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Navbar, Nav, Form, FormControl, Container } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-    
+      <Navbar bg="primary" expand="lg" variant='dark'>
+        <Container fluid>
+          <Navbar.Brand href="#">Blogs</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+              <Nav.Link href="#action1">Home</Nav.Link>
+              <Nav.Link href="#action2">Link</Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <FormControl
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 }
