b49a6d7060ae996cb3955eeb30a7722c395b28e1
punkestu <pangestubima89@gmail.com>
2022-04-21 13:12:50 +0700

    adding segmentation on blogs view

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,18 +7,25 @@ export default function () {
 
       const [blogs, setBlogs] = React.useState({});
       const [loaded, setLoaded] = React.useState(false);
+      const [segment, setSegment] = React.useState(1);
+      const [nSegment, setNSegment] = React.useState(1);
       React.useEffect(() => {
-            fetch("https://6260dcb4e7361dff91fb310d.mockapi.io/blogs", { method: "get" }).then(res => { return res.json() }).then(res => { setBlogs(res); setLoaded(true); });
+            fetch("https://6260dcb4e7361dff91fb310d.mockapi.io/blogs", { method: "get" }).then(res => { return res.json() }).then(res => {
+                  setBlogs(res);
+                  setLoaded(true);
+                  setNSegment(Math.floor(res.length / 6));
+            });
       }, []);
       function cut(text) {
             // console.log(text[0])
-            if (text.length >= 100) {
-                  return text.substring(0, 100) + " ...";
+            if (text.length >= 50) {
+                  return text.substring(0, 50) + " ...";
             } else return text
       }
-      return <div className="d-flex flex-wrap m-2 justify-content-center">
+      return <>
+            <Row className="d-flex flex-wrap m-2 justify-content-center">
                   {loaded ?
-                        blogs.map(element => {
+                        blogs.slice((segment-1)*6, segment * 6).map(element => {
                               return (
                                     <Card key={element.id} className="w-25 text-start m-1 py-2 px-3">
                                           <Card.Img variant="top" src={element.pic} />
@@ -34,6 +41,19 @@ export default function () {
                         })
                         : <div>Loading</div>
                   }
-                  {/* </Col> */}
-      </div>;
+            </Row>
+            <div className="d-flex justify-content-center align-items-center">
+                  <Button onClick={()=>{
+                        if(segment>1){
+                              setSegment(segment-1);
+                        }
+                  }}>&lt;</Button>
+                  <div className="text-dark mx-4">{segment}/{nSegment}</div>
+                  <Button onClick={()=>{
+                        if(segment<nSegment){
+                              setSegment(segment+1);
+                        }
+                  }}>&gt;</Button>
+            </div>
+      </>;
 };
